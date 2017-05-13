2612f4a16a86a4f99c877cf0f0b6aca8f02730d0
Kerzzi <kerzzi@outlook.com>
2017-05-14 00:25:07 +0800

    完成分类管理开发

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,67 @@
 class Admin::CategoriesController < Admin::BaseController
+  before_action :find_root_categories, only: [:new, :create, :edit, :update]
+  before_action :find_category, only: [:edit, :update, :destroy]
+
   def index
+    if params[:id].blank?
+      @categories = Category.roots
+    else
+      @category = Category.find(params[:id])
+      @categories = @category.children
+    end
+
+    @categories = @categories.page(params[:page] || 1).per_page(params[:per_page] || 10)
+      .order(id: "desc") #这个也记住，改成全栈营中的代码
   end
 
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(params.require(:category).permit!)
+    #以哈希类型的属性，必须做一个过滤，防止批量的属性攻击
+    #permit!意味着与数据库对应的所有的属性都接收，这不是一种严格的写法
+
+    if @category.save
+      flash[:notice] = "保存成功"
+      redirect_to admin_categories_path
+    else
+      render action: :new
+    end
+  end
+
+  def edit
+    render action: :new
+  end
+
+  def update
+    @category.attributes = params.require(:category).permit!
+
+    if @category.save
+      flash[:notice] = "修改成功"
+      redirect_to admin_categories_path
+    else
+      render action: :new
+    end
+  end
+
+  def destroy
+    if @category.destroy
+      flash[:notice] = "删除成功"
+      redirect_to admin_categories_path
+    else
+      flash[:notice] = "删除失败"
+      redirect_to :back  #返回来源页面，这样用户体验会更好
+    end
+  end
+
+  private
+  def find_root_categories
+    @root_categories = Category.roots.order(id: "desc")
+  end
+
+  def find_category
+    @category = Category.find(params[:id])
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,16 @@
 class Category < ApplicationRecord
 
-  has_ancestry
+  validates :title, presence: { message: "名称不能为空" }
+  validates :title, uniqueness: { message: "名称不能重复" }
+
+  has_ancestry orphan_strategy: :destroy  #删除一级分类时，二级分类自动删除
 
   has_many :products, dependent: :destroy
+
+  before_validation :correct_ancestry
+
+  private
+  def correct_ancestry
+    self.ancestry = nil if self.ancestry.blank?
+  end
 end

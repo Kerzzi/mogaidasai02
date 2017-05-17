c366235af55a72fc87c37e2b6f82e41252c0f9c2
Kerzzi <kerzzi@outlook.com>
2017-05-17 21:42:36 +0800

    管理员角色功能开发

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -2,5 +2,14 @@ class Admin::BaseController < ActionController::Base
 
   # 这样admin中的controller，都继承自这个类，都使用admin布局
   layout 'admin/layouts/admin'
+  
+  before_action :auth_admin
 
+  private
+  def auth_admin
+    unless logged_in? and current_user.is_admin?
+      flash[:notice] = "请以管理员身份登录"
+      redirect_to new_session_path
+    end
+  end
 end

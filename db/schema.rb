1ab64d2cb47ac725ab93f402ec6ec20baeb477f2
Kerzzi <kerzzi@outlook.com>
2017-05-17 21:30:28 +0800

    add_users_is_admin_column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170517100321) do
+ActiveRecord::Schema.define(version: 20170517132845) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer  "user_id"
@@ -91,11 +91,11 @@ ActiveRecord::Schema.define(version: 20170517100321) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                           null: false
+    t.string   "email",                                           null: false
     t.string   "crypted_password"
     t.string   "salt"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
     t.string   "activation_state"
     t.string   "activation_token"
     t.datetime "activation_token_expires_at"
@@ -106,6 +106,7 @@ ActiveRecord::Schema.define(version: 20170517100321) do
     t.datetime "reset_password_email_sent_at"
     t.string   "uuid"
     t.integer  "default_address_id"
+    t.boolean  "is_admin",                        default: false
     t.index ["activation_token"], name: "index_users_on_activation_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
